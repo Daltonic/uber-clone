2a01378dbabc52054316a283fc96afddce0dad88
Daltonic <darlingtongospel@gmail.com>
2021-11-01 15:40:00 +0100

    setup the redux store and Home Screen structure

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import HomeScreen from './screens/HomeScreen'
+import { store } from './store'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Building Uber!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +20,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello To The Home Screen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
